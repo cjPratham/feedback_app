a1918ed3e4cbeed2f773245bc9b8095f5c3ccf9a
cjPratham <pratham007yadav@gmail.com>
2025-07-26 14:49:09 +0530

    api url updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,35 @@ import FeedbackList from './components/FeedbackList';
 function App() {
   const [feedbacks, setFeedbacks] = useState([]);
 
-  // Fetch from backend
+  // // Fetch from backend locally
+  // useEffect(() => {
+  //   fetch('http://localhost:5000/api/feedback')
+  //     .then((res) => res.json())
+  //     .then((data) => setFeedbacks(data))
+  //     .catch((err) => console.error('Error:', err));
+  // }, []);
   useEffect(() => {
-    fetch('http://localhost:5000/api/feedback')
+    fetch('https://feedback-app-qmkw.onrender.com/api/feedback')
       .then((res) => res.json())
       .then((data) => setFeedbacks(data))
       .catch((err) => console.error('Error:', err));
   }, []);
 
-  // Submit to backend
+  // Submit to backend to call local API
+  // const addFeedback = async (feedback) => {
+  //   const res = await fetch('http://localhost:5000/api/feedback', {
+  //     method: 'POST',
+  //     headers: { 'Content-Type': 'application/json' },
+  //     body: JSON.stringify({ text: feedback.text, rating: 5 }), // add rating if needed feedback.rating
+  //   });
+
+  //   const data = await res.json();
+  //   setFeedbacks([data, ...feedbacks]);
+  // };
+
+
   const addFeedback = async (feedback) => {
-    const res = await fetch('http://localhost:5000/api/feedback', {
+    const res = await fetch('https://feedback-app-qmkw.onrender.com/api/feedback', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ text: feedback.text, rating: 5 }), // add rating if needed feedback.rating
